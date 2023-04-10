b881e60c6ca371ce59b8e7dc174de78f97ff8b15
Harshh04 <92075660+Harshh04@users.noreply.github.com>
2023-04-10 12:13:44 +0530

    Update app.js
    Now added mongoose online DB

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 //jshint esversion:6
 
-const posts = [] ;
-
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-const _  = require('lodash');
+const _ = require("lodash");
+const mongoose = require("mongoose") ;
+
+mongoose.connect("mongodb+srv://harshin416:Openit004@cluster0.lf00ccc.mongodb.net/?retryWrites=true&w=majority",{useNewUrlParser: true, useUnifiedTopology: true})
+  .then(() => console.log("Database connection established successfully!"))
+  .catch(err => console.error("Database connection error:", err));
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -13,70 +16,53 @@ const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rho
 
 const app = express();
 
+const postSchema = {title : String , content : String} ; //Schema defined 
+
+const Post = mongoose.model("Post",postSchema) //model created post with Schema of postSchema
+
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-app.get("/",function(req,res)
-{
-  res.render("home", 
-  {startingContent : homeStartingContent,
-    posts : posts
-    }) ;
-})
+app.get("/", function(req, res)
+{ Post.find({})
+  .then(posts=>res.render("home", {
+  startingContent: homeStartingContent,
+  posts: posts,
+  }))
+}
+);
 
-app.get("/about",function(req,res)
-{
-  res.render("about", {startingContent : aboutContent}) ;
-})
+app.get("/about", function(req, res){
+  res.render("about", {aboutContent: aboutContent});
+});
 
-app.get("/contact",function(req,res)
-{
-  res.render("contact", {startingContent : contactContent}) ;
-})
+app.get("/contact", function(req, res){
+  res.render("contact", {contactContent: contactContent});
+});
 
-app.get("/compose",function(req,res)
-{
-  res.render("compose", {startingContent : contactContent}) ;
-})
+app.get("/compose", function(req, res){
+  res.render("compose");
+});
 
-app.post("/compose",function(req,res)
-{
-  const post = { title : req.body.postTitle , 
-                content : req.body.postBody}
+app.post("/compose", function(req, res){
+  const post = new Post({
+    title: req.body.postTitle,
+    content: req.body.postBody
+  });  //new post created
 
-  posts.push(post) ;
-  res.redirect("/compose") ;
-})
+  post.save() ;
 
-app.get("/post/:postName" , function(req,res)
-{
-  let requestedTitle = req.params.postName ; 
-  requestedTitle = _.lowerCase(requestedTitle) ;
-  posts.forEach(function(post)
-  
-    {
-      let storedTitle = post.title ;
-      storedTitle = _.lowerCase(storedTitle) ;
-      if(storedTitle === requestedTitle)
-      {
-        res.render("post",{post : post})
-      }
-    }
-  ) 
-})
+  res.redirect("/");
 
-app.get
+});
 
-
-
-
-
-
-
-
-
+app.get("/posts/:postId", function(req, res){
+  const requestedTitle = req.params.postId;
+  Post.findOne({_id:requestedTitle})
+  .then(post=>{res.render("post",{title : post.title , content : post.content})})
+});
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
